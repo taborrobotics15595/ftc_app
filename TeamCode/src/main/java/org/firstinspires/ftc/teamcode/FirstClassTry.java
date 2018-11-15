cf043ed8eddc6072b0ace6447538c459dca2e00e
DESKTOP-VS0892C\jsula <j.sulaimanm@icloud.com>
2018-11-15 18:17:41 -0500

    try

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstClassTry.java
@@ -0,0 +1,38 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+@TeleOp
+//@Disabled
+public class FirstClassTry extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
